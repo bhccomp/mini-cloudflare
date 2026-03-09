3096828fae94bd7d6e62e85dde76ab0e633f77f0
bhccomp <bhccomp@gmail.com>
2026-03-09 23:47:22 +0100

    fix(wordpress): use correct sample table name

--- app/Models/WordPressSignatureSample.php
+++ app/Models/WordPressSignatureSample.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class WordPressSignatureSample extends Model
 {
+    protected $table = 'wordpress_signature_samples';
+
     protected $fillable = [
         'name',
         'sample_type',
